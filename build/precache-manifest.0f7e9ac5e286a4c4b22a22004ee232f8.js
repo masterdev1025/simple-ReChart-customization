8aaffa907a59d8133ed51d1d3170f405f718c02b
ikramcoder1988 <ikramcoder1988@gmail.com>
2020-03-08 01:10:25 +0800

    first-commit

---
+++ build/precache-manifest.0f7e9ac5e286a4c4b22a22004ee232f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec524f9e506d5adde730a2fafb441b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af6132f56b405a748c15",
+    "url": "/static/css/main.86fccd8d.chunk.css"
+  },
+  {
+    "revision": "4565353b42d434d44500",
+    "url": "/static/js/2.6640e945.chunk.js"
+  },
+  {
+    "revision": "977b85ae92e0fb68aebdece70c5a2518",
+    "url": "/static/js/2.6640e945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6132f56b405a748c15",
+    "url": "/static/js/main.d67a6a0a.chunk.js"
+  },
+  {
+    "revision": "8fce50ef7d01a5134ada",
+    "url": "/static/js/runtime-main.88233cf2.js"
+  }
+]);
